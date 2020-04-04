5fb3564cb74443a031f49cb50e26c7e4710849c9
Amit Attri <amitattri@urbanclap.com>
2020-04-04 15:51:01 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import VideoPlayBack from './Components/VideoPlayBack/index'
+import './App.css';
+import LiveStream from './Components/LiveStream/LiveStream';
+
+function App() {
+  
+  return (
+    <div>
+      {/* <VideoPlayBack /> */}
+      <LiveStream />
+    </div>
+  );
+}
+
+export default App;
